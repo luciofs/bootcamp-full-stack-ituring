f919825e2a1170434f80ae3490bbd6026714caab
Lucio <LUCIOFLASAN@GMAIL.COM>
2022-10-09 19:05:37 -0300

    adicionando alterações ao pratica-1

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,11 @@
+//declare 5 variáveis que vão armazenar os seguintes valores:
+//seu nome
+let nome=("Lucio")
+//seu email
+let email=("lucioflasan@gmail.com")
+//sua profissão
+let prof=("Analista de TI")
+//sua idade
+let idade=(49)
+//o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
+let local=("Síria")
