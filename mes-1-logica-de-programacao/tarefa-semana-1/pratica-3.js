487e64f629b48d305e4ce28bbc711a1a7056fe3a
Lucio <LUCIOFLASAN@GMAIL.COM>
2022-10-09 21:33:28 -0300

    praticando no arquivo pratica-3

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,43 @@
+//declare 5 variáveis que vão armazenar os seguintes valores:
+//seu nome
+let nome=("Lucio")
+//seu email
+let email=("lucioflasan@gmail.com")
+//sua profissão
+let prof=("Analista de TI")
+//sua idade
+let idade=(49)
+//o lugar que você sonha um dia conhecer (pode ser um país, cidade ou um local mesmo)
+let local=("Síria")
+
+//Coloque-as no arquivo pratica-3.js e exiba o valor true com console.log() se a variável for do tipo string e false se a variável não for do tipo number.
+
+if (typeof nome == "string") {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+if (typeof email == "string") {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+if (typeof prof == "string") {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+if (typeof idade == "string") {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+if (typeof local == "string") {
+    console.log(true)
+} else {
+    console.log(false)
+}
