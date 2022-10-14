80a7230231018b31e93c9cea309970dfac62941a
Lucio <LUCIOFLASAN@GMAIL.COM>
2022-10-14 16:07:49 -0300

    finalizando o arquivo pratica-3.js

--- mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-3.js
@@ -1,12 +1,26 @@
-//Criar uma função que retorne um número aleatório entre 0 e 10000
+//Criar uma função SEM PARAMETRO que retorne um número aleatório entre 0 e 10000
 
+function retornaNumero () {
+    const min = 0
+    const max = 10000
+    
+    numeroEncontrado = Math.floor(Math.random()*(max - min)) + min;
+    
+    return numeroEncontrado;
+}
+
+console.log("O número escolhido aleatóriamente foi: "+retornaNumero ())
+
+
+//Função COM PARAMETRO sem retorno, MOSTRANDO O NUMERO DIGITADO e O NÚMERO SORTEADO
 function mostraNumero (numeroDigitado) {
     const min = 0
     const max = 10000
     
     numeroEncontrado = Math.floor(Math.random()*(max - min)) + min;
-    console.log("O número que voce digitou foi:"+numeroDigitado);
+    console.log("\nO número que voce digitou foi:"+numeroDigitado);
     console.log("O número sorteado foi: "+numeroEncontrado);
+    
 }
 
-mostraNumero (2555)
+mostraNumero (1004)
