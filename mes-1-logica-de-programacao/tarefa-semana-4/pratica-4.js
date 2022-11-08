8116466b0f201918c95ef2982ee5c50945e81b7b
Lucio <LUCIOFLASAN@GMAIL.COM>
2022-11-07 21:40:39 -0300

    praticando tarefa-4 pratica-4 e pratica-5

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-4.js
@@ -0,0 +1,7 @@
+/*escreva uma função que recebendo uma lista com valores do tipo number, retorne uma lista com a raiz quadrada de cada um dos elementos. Qual método de iteração é o mais apropriado para este problema? .forEach(), .map() ou .filter()? Utilize o método mais apropriado para este problema.*/
+
+let lista = [4, 9, 16, 25, 36, 49]
+let novaLista = lista.map(function(item) {
+    
+    return novaLista;
+  })
---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-5.js
@@ -0,0 +1,9 @@
+/*Ordenando uma lista de números
+Bom, na tarefa da semana passada, você desenvolveu um algoritmo de ordenação! Agora você irá usar o algoritmo de ordenação padrão do JavaScript. Escreva uma função no arquivo pratica-5.js que, dado uma lista de números, retorne a lista em ordem crescente de valor.
+
+*/
+
+lista = [4, 9, 7, 1, 8, 12]
+listaExemplo.forEach(function(item) {
+    console.log(`Valor: ${item} | Tipo: ${typeof(item)}`)
+}
